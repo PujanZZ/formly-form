62391f2df5b3e2ec92d462bcbf990211cf80c91e
PujanZZ <71079402+PujanZZ@users.noreply.github.com>
2023-02-28 20:22:56 +0530

    2 files modified

--- src/app/dragdrop/dragdrop.component.ts
+++ src/app/dragdrop/dragdrop.component.ts
@@ -1,4 +1,8 @@
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -13,19 +17,28 @@ export class DragdropComponent implements OnInit {
     { title: 'Item 3', description: 'This is item 3.' },
   ];
 
-  items2 = [];
+  items2 = [
+    { title: 'Item 4', description: 'This is item 4.' },
+    { title: 'Item 5', description: 'This is item 5.' },
+    { title: 'Item 6', description: 'This is item 6.' },
+  ];
 
   constructor() {}
 
   ngOnInit() {}
 
   dropItem(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer !== event.container) {
-      const item = event.previousContainer.data[event.previousIndex];
-      event.previousContainer.data.splice(event.previousIndex, 1);
-      event.container.data.splice(event.currentIndex, 0, item);
-    } else {
+    if (event.previousContainer === event.container) {
+      // Move the item within the same list
       moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      // Remove the item from the original list and add it to the new list
+      transferArrayItem(
+        event.previousContainer.data,
         event.container.data,
         event.previousIndex,
         event.currentIndex
